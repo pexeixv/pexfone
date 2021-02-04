f4d9ded7d85360cf7664acc11a4d8388d79caee7
pexeixv <gavineditz@gmail.com>
2021-02-04 16:06:46 +0530

    js improved on phone.js

--- js/phone.js
+++ js/phone.js
@@ -1,73 +1,39 @@
-let imagePlaceholder = document.querySelector('#defaultImage');
-let onClickImage1 = document.querySelector('#onClickImage1');
-let onClickImage2 = document.querySelector('#onClickImage2');
-let onClickImage3 = document.querySelector('#onClickImage3');
-let onClickImage4 = document.querySelector('#onClickImage4');
-let onClickImage5 = document.querySelector('#onClickImage5');
-let onClickImage6 = document.querySelector('#onClickImage6');
-let currentPath = '../img/phone/ca/caFront.png'
+let placeholder = document.querySelector('#defaultImage');
+let currentPath = placeholder.src
+let images = document.querySelectorAll('.onClickImage');
 
-onClickImage1.addEventListener('click', () => {
-    imagePlaceholder.src = '../img/phone/ca/caFront.png';
-    currentPath = '../img/phone/ca/caFront.png';
+// Grayscale when not focused
+function gray(){
+images.forEach(image=>{
+    if(image.src!==currentPath)
+        image.style.filter = 'grayscale(1)';
 })
-onClickImage2.addEventListener('click', () => {
-    imagePlaceholder.src = '../img/phone/ca/caBack.png';
-    currentPath = '../img/phone/ca/caBack.png';
-})
-onClickImage3.addEventListener('click', () => {
-    imagePlaceholder.src = '../img/phone/ca/caRight.png';
-    currentPath = '../img/phone/ca/caRight.png';
-})
-onClickImage4.addEventListener('click', () => {
-    imagePlaceholder.src = '../img/phone/ca/caLeft.png';
-    currentPath = '../img/phone/ca/caLeft.png';
-})
-onClickImage5.addEventListener('click', () => {
-    imagePlaceholder.src = '../img/phone/ca/caDiagL.png';
-    currentPath = '../img/phone/ca/caDiagL.png';
-})
-onClickImage6.addEventListener('click', () => {
-    imagePlaceholder.src = '../img/phone/ca/caDiagR.png';
-    currentPath = '../img/phone/ca/caDiagR.png';
+}
+
+gray();
+
+// onClick event handler
+images.forEach(image=>{
+    image.addEventListener('click',()=>{
+        placeholder.src=currentPath=image.src;
+        gray();
+    })
 })
 
+// onHover event handler
+images.forEach( image => {
+    image.addEventListener('mouseenter', () => {
+            placeholder.src = image.src;
+            image.style.filter = 'grayscale(0)';
+        });
+    });
 
-onClickImage1.addEventListener('mouseenter', () => {
-    imagePlaceholder.src = '../img/phone/ca/caFront.png';
-})
-onClickImage1.addEventListener('mouseleave', () => {
-    imagePlaceholder.src = currentPath
-})
-onClickImage2.addEventListener('mouseenter', () => {
-    imagePlaceholder.src = '../img/phone/ca/caBack.png';
-})
-onClickImage2.addEventListener('mouseleave', () => {
-    imagePlaceholder.src = currentPath
-})
-onClickImage3.addEventListener('mouseenter', () => {
-    imagePlaceholder.src = '../img/phone/ca/caRight.png';
-})
-onClickImage3.addEventListener('mouseleave', () => {
-    imagePlaceholder.src = currentPath
-})
-onClickImage4.addEventListener('mouseenter', () => {
-    imagePlaceholder.src = '../img/phone/ca/caRight.png';
-})
-onClickImage4.addEventListener('mouseleave', () => {
-    imagePlaceholder.src = currentPath
-})
-onClickImage5.addEventListener('mouseenter', () => {
-    imagePlaceholder.src = '../img/phone/ca/caDiagL.png';
-})
-onClickImage5.addEventListener('mouseleave', () => {
-    imagePlaceholder.src = currentPath
-})
-onClickImage6.addEventListener('mouseenter', () => {
-    imagePlaceholder.src = '../img/phone/ca/caDiagR.png';
-})
-onClickImage6.addEventListener('mouseleave', () => {
-    imagePlaceholder.src = currentPath
-})
+// stopHover event handler
+images.forEach( image => {
+    image.addEventListener('mouseleave', () => {
+            placeholder.src = currentPath;
+            gray();
+        });
+    });
 
 
