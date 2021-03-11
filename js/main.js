18e302ae8c1addc92af4d8f3281df5ec3ff334a1
Gavin Pereira <gavineditz@gmail.com>
2021-03-11 21:49:02 +0530

    home made responsive, hamb added for all

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.querySelector('.bars').addEventListener('click', () => {
+    document.querySelector('header ul').classList.toggle('show');
+    document.querySelector('.bars').classList.toggle('cross');
+})
+
+
