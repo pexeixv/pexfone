d50e943528a6f8db4925507974a5cb4776a1b494
Gavin Pereira <gavineditz@gmail.com>
2021-03-12 13:29:43 +0530

    products appended from template tag

--- js/phone.js
+++ js/phone.js
@@ -3,44 +3,49 @@ let currentPath = placeholder.src
 let images = document.querySelectorAll('.onClickImage');
 
 // Grayscale when not focused
-function gray(){
-images.forEach(image=>{
-    if(image.src!==currentPath)
-        image.style.filter = 'grayscale(1)';
-})
+function gray() {
+    images.forEach(image => {
+        if (image.src !== currentPath)
+            image.style.filter = 'grayscale(1)';
+    })
 }
 
 gray();
 
 // onClick event handler
-images.forEach(image=>{
-    image.addEventListener('click',()=>{
-        placeholder.src=currentPath=image.src;
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        placeholder.src = currentPath = image.src;
         gray();
     })
 })
 
 // onHover event handler
-images.forEach( image => {
+images.forEach(image => {
     image.addEventListener('mouseenter', () => {
-            placeholder.src = image.src;
-            image.style.filter = 'grayscale(0)';
-        });
+        placeholder.src = image.src;
+        image.style.filter = 'grayscale(0)';
     });
+});
 
 // stopHover event handler
-images.forEach( image => {
+images.forEach(image => {
     image.addEventListener('mouseleave', () => {
-            placeholder.src = currentPath;
-            gray();
-        });
+        placeholder.src = currentPath;
+        gray();
     });
+});
 
 
 window.addEventListener("scroll", (e) => {
     let header = document.querySelector('header');
     let scroll = this.scrollY;
-    if(scroll>=50)
+    if (scroll >= 50)
         header.style.boxShadow = '0 2px 10px 2px rgba(0,0,0,0.2)';
     else header.style.boxShadow = 'none';
 });
+
+
+
+
+
