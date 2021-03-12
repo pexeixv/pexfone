bd41a451c0b2c93590a3a2c731f54b5702f846ef
Gavin Pereira <gavineditz@gmail.com>
2021-03-12 09:01:22 +0530

    hamb test

--- js/main.js
+++ js/main.js
@@ -4,13 +4,6 @@ document.querySelector('.bars').addEventListener('click', () => {
 })
 
 document.querySelector('.bars').addEventListener('touchstart', () => {
-    document.querySelector('header ul').classList.add('show');
-    document.querySelector('.bars').classList.add('cross');
+    document.querySelector('header ul').classList.toggle('show');
+    document.querySelector('.bars').classList.toggle('cross');
 })
-
-document.querySelector('.bars').addEventListener('touchend', () => {
-    document.querySelector('header ul').classList.remove('show');
-    document.querySelector('.bars').classList.remove('cross');
-})
-
-
