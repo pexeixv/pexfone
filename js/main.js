453658e6fc0876075564834667982e691591f29e
Gavin Pereira <gavineditz@gmail.com>
2021-03-16 12:51:55 +0530

    idk really

--- js/main.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.bars').addEventListener('click', () => {
-    document.querySelector('header ul').classList.toggle('show');
-    document.querySelector('.bars').classList.toggle('cross');
-})
